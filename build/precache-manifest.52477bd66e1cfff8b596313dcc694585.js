8428325153b2f7cc021ef222738a397384bf7411
Justin Stefan <justin.popa20@gmail.com>
2020-05-27 16:25:07 +0300

    Update gh pages

---
+++ build/precache-manifest.52477bd66e1cfff8b596313dcc694585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd5a8ddd414c366d3d3ca5d5bbd8996",
+    "url": "/layout-template-builder/index.html"
+  },
+  {
+    "revision": "7d5a55c932643ddcf8a4",
+    "url": "/layout-template-builder/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c4f86bc269f4272f6c17",
+    "url": "/layout-template-builder/static/js/2.d1243d9e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/layout-template-builder/static/js/2.d1243d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5a55c932643ddcf8a4",
+    "url": "/layout-template-builder/static/js/main.e16f2eae.chunk.js"
+  },
+  {
+    "revision": "2a48b7db896406559203",
+    "url": "/layout-template-builder/static/js/runtime-main.a82cd157.js"
+  }
+]);
