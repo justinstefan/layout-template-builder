e52bed1a7dab7152e0c4af0e41b1bb922a065082
Justin Stefan <justin.popa20@gmail.com>
2020-05-27 16:05:46 +0300

    Test gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Configuration from "./components/Configuration";
+import Templates from "./components/Templates";
+
+import { Route, Switch } from "react-router";
+
+let routes = (
+  <Switch>
+    <Route exact path="/">
+      <Templates />
+    </Route>
+    <Route path="/:layout">
+      <Configuration />
+    </Route>
+  </Switch>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div>{routes}</div>
     </div>
   );
 }
